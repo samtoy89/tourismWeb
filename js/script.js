7595a6941960fa2ca6fa305ed9c513644887b191
sam omotay <you@example.com>
2021-11-30 01:14:08 +0000

    third commit

--- js/script.js
+++ js/script.js
@@ -1,133 +1,133 @@
 let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
+document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.add('active');
 }
 
-document.querySelector('#nav-close').onclick = () =>{
+document.querySelector('#nav-close').onclick = () => {
     navbar.classList.remove('active');
 }
 
 let searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
+document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.add('active');
 }
 
-document.querySelector('#close-search').onclick = () =>{
+document.querySelector('#close-search').onclick = () => {
     searchForm.classList.remove('active');
 }
 
-window.onscroll = () =>{
+window.onscroll = () => {
     navbar.classList.remove('active');
 
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         document.querySelector('.header').classList.add('active');
-    }else{
+    } else {
         document.querySelector('.header').classList.remove('active');
     }
 };
 
-window.onload = () =>{
-    if(window.scrollY > 0){
+window.onload = () => {
+    if (window.scrollY > 0) {
         document.querySelector('.header').classList.add('active');
-    }else{
+    } else {
         document.querySelector('.header').classList.remove('active');
     }
 };
 
 
 var swiper = new Swiper(".home-slider", {
-    loop:true, 
-    grabCursor:true,
+    loop: true,
+    grabCursor: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });
 
 var swiper = new Swiper(".product-slider", {
-    loop:true, 
-    grabCursor:true,
+    loop: true,
+    grabCursor: true,
     spaceBetween: 20,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     breakpoints: {
         0: {
-          slidesPerView: 1,
+            slidesPerView: 1,
         },
         640: {
-          slidesPerView: 2,
+            slidesPerView: 2,
         },
         768: {
-          slidesPerView: 3,
+            slidesPerView: 3,
         },
         1024: {
-          slidesPerView: 4,
+            slidesPerView: 4,
         },
     },
 });
 
 var swiper = new Swiper(".review-slider", {
-    loop:true, 
-    grabCursor:true,
+    loop: true,
+    grabCursor: true,
     spaceBetween: 20,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     breakpoints: {
         0: {
-          slidesPerView: 1,
+            slidesPerView: 1,
         },
         640: {
-          slidesPerView: 2,
+            slidesPerView: 2,
         },
         768: {
-          slidesPerView: 3,
+            slidesPerView: 3,
         },
     },
 });
 
 var swiper = new Swiper(".blogs-slider", {
-    loop:true, 
-    grabCursor:true,
+    loop: true,
+    grabCursor: true,
     spaceBetween: 10,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     breakpoints: {
         0: {
-          slidesPerView: 1,
+            slidesPerView: 1,
         },
         768: {
-          slidesPerView: 2,
+            slidesPerView: 2,
         },
         991: {
-          slidesPerView: 3,
+            slidesPerView: 3,
         },
     },
 });
 
 var swiper = new Swiper(".clients-slider", {
-    loop:true, 
-    grabCursor:true,
+    loop: true,
+    grabCursor: true,
     spaceBetween: 20,
     breakpoints: {
         0: {
-          slidesPerView: 1,
+            slidesPerView: 1,
         },
         640: {
-          slidesPerView: 2,
+            slidesPerView: 2,
         },
         768: {
-          slidesPerView: 3,
+            slidesPerView: 3,
         },
         1024: {
-          slidesPerView: 4,
+            slidesPerView: 4,
         },
     },
 });
